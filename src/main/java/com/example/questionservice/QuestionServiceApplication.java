297cc9e8abd6a31e873afdf1da903f1b8829fe20
ManojKannanD <fabulousmanoj27@gmail.com>
2024-04-27 09:57:39 +0530

    connected to eureka-server

--- src/main/java/com/example/questionservice/QuestionServiceApplication.java
+++ src/main/java/com/example/questionservice/QuestionServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.questionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class QuestionServiceApplication {
 
 	public static void main(String[] args) {
